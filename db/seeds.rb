452302bc9e01ed7879562eb6eca50dd8457a16f0
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-20 15:16:39 +1000

    disabled to use json as default format for all pages. Removed incorrect way to generate seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-Event.destroy_all
-
-10.times do |index|
-  Event.create!(date: Faker::Time,
-                venue_id: Faker::Number,
-                ticket_url: Faker::Internet.url,
-                image: Faker::Internet.url,
-                )
-end
