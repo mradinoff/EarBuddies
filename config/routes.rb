d4b3f7adde6915f85f76a23216fd68aa48f7d066
Taryn <taryn@sugarpush.com.au>
2018-04-20 12:50:09 +1000

    starting to set up friendships

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :venues
   resources :events
   resources :users
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
