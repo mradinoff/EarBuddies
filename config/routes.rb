4ec94ebf180494132e81167bf922bdf4e64a3a4d
Joseph Ocampo <josephocampo@MacBook-Pro-6.local>
2018-04-23 19:01:38 +1000

    added events_user model and scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   resources :friendships
+  resources :events_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
