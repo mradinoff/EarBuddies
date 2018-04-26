d70770622377901dfef57fbf2a4fef5757222a3d
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-26 10:54:30 +1000

    fixed typo for action cable ws production url.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://earbuddies1.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://earbuddies1.herokuapp.com' ]
+  #config.action_cable.allowed_request_origins = [ 'https://earbuddies1.herokuapp.com' ]
+
+config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
