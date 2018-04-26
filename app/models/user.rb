ec001164e66dd6de2a04ea0631d1cc30031d1369
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-26 21:59:52 +1000

    simple validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,9 @@ class User < ApplicationRecord
   has_many :friends, :through => :friendships
   has_many :messages
 
+  validates :email, presence: true
+  validates :password, confirmation: true
+
 
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
